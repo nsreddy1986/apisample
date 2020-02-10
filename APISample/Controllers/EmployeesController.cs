4cc8690eaeb6caedad2ff27f218016976fd1c386
Seshi Reddy Nangu <nsreddy1986@gmail.com>
2020-02-10 17:43:26 +0530

    local commit

---
+++ APISample/Controllers/EmployeesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APISample.models;
+using APISample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APISample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // GET api/Employees
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return EmployeeService.Employees;
+        }
+
+        ////GET api/Employees/id
+        //[HttpGet]
+        //public ActionResult<Employee> Get(int id)
+        //{
+        //    return EmployeeService.Employees.First(e=>e.Id == id);
+        //}
+
+        [HttpPost]
+        public ActionResult<Employee> Post(Employee employee)
+        {
+            EmployeeService.Add(employee);
+            return employee;
+        }
+
+        [HttpDelete]
+        public ActionResult<Employee> Delete(int id)
+        {
+            return EmployeeService.Delete(id);
+        }
+
+        [HttpPut]
+        public ActionResult<Employee> Put(Employee employee)
+        {
+            EmployeeService.Update(employee);
+            return employee;
+        }
+    }
+}
---
+++ APISample/Services/EmployeeService.cs
@@ -0,0 +1,52 @@
+﻿using APISample.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APISample.Services
+{
+    public static class EmployeeService
+    {
+        public static List<Employee> Employees { get; set; }
+        static EmployeeService()
+        {
+            Employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id=1,
+                    FirstName = "Seshi",
+                    LastName = "Reddy",
+                    Designation = "Technology Specialist"
+                },
+                new Employee
+                {
+                    Id=2,
+                    FirstName = "Nandini",
+                    LastName = "Durga",
+                    Designation = "Software Engineer"
+                }
+            };
+        }
+        public static void Add(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+        public static Employee Delete(int id)
+        {
+            var emp = Employees.First(e => e.Id == id);
+            Employees.Remove(emp);
+            return emp;
+        }
+        public static Employee Update(Employee employee)
+        {
+            var emp=Employees.First(e=>e.Id == employee.Id);
+            emp.FirstName = employee.FirstName;
+            emp.LastName = employee.LastName;
+            emp.Designation = employee.Designation;
+            return emp;
+        }
+
+    }
+}
